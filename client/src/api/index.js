5febcee7ae240854b9228a30eba955b3e72a78c8
Abdallah Farag <abdallahfarag72@gmail.com>
2022-12-06 18:23:36 +0200

    migrate to vercel & netlify

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-const customersURL = 'https://azzam-banking-api.herokuapp.com/customers'
+const customersURL = 'https://tsf-banking-app-api.vercel.app/customers'
 
 export const fetchCustomers = () => axios.get(customersURL)
 export const updateCustomers = (senderId, receiverId, amount) => axios.patch(`${customersURL}/${senderId}?receiver=${receiverId}&amount=${amount}`)
 
 
-const transfersURL = 'https://azzam-banking-api.herokuapp.com/transfers'
+const transfersURL = 'https://tsf-banking-app-api.vercel.app/transfers'
 
 export const fetchTransfers = () => axios.get(transfersURL)
 export const createTransfer = (transfer) => axios.post(transfersURL, transfer)
